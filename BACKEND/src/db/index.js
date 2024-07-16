5fc741738a07f962bf201695ad1427edfa571cf8
AKSHATH P <akshathpkk@gmail.com>
2024-07-16 21:12:52 +0530

    first commit - configured backend , db and server connected

---
+++ BACKEND/src/db/index.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "public\temp.env" });
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const MONGO_PASSWORD = "akshath04";
+
+const connectDB = async () => {
+  try {
+    console.log(
+      `Connection String: mongodb+srv://akshath:${MONGO_PASSWORD}@cluster0.kek2ckn.mongodb.net/${DB_NAME}`
+    );
+
+    const connectionInstance = await mongoose.connect(
+      `mongodb+srv://akshath:${MONGO_PASSWORD}@cluster0.kek2ckn.mongodb.net/${DB_NAME}`
+    );
+    console.log(
+      `\n  MONGO DB CONNECTED .. DB HOST : ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("connection error : ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
