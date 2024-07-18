f4e14de70958a6ab816208083fab722a321f06a4
AKSHATH P <akshathpkk@gmail.com>
2024-07-18 14:58:38 +0530

    bug fixes

--- BACKEND/src/middlewares/auth.middleware.js
+++ BACKEND/src/middlewares/auth.middleware.js
@@ -5,13 +5,15 @@ import {Mentor} from "../models/mentor.model.js";
 
 export const verifyJWT = asyncHandler(async(req , _ ,next)=>{
     try{
-        const token = req.cookie?.accessToken ||  req.header("Authorization")?.replace("Bearer ", "")
+        console.log(req.cookies?.accessToken)
+        const token = req.cookies?.accessToken ||  req.header("Authorization")?.replace("Bearer ", "")
 
         if(!token){
             throw new ApiError(401 , "unauthorised request");
         }
 
         const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET);
+        console.log(decodedToken._id)
 
         const user = await Mentor.findById(decodedToken?._id).select("-password -refreshToken");
 
