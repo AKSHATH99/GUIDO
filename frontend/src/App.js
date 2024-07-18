eedf035fd62c2a33b5d24dac01dd6f17f1aa1028
AKSHATH P <akshathpkk@gmail.com>
2024-07-18 15:09:59 +0530

    configured reactjs and tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className = "text-blue-700">GUIDO LOADING BITCH </p>
+    </div>
+  );
+}
+
+export default App;
