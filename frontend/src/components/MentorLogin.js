b927b42d29b38040f2175281a3b541020d0a107c
AKSHATH P <akshathpkk@gmail.com>
2024-07-19 19:46:27 +0530

    added login and register

---
+++ frontend/src/components/MentorLogin.js
@@ -0,0 +1,73 @@
+import React , {useState} from "react";
+import axios from "axios";
+
+const MentorLogin = () => {
+  const [formdata, setFormdata] = useState({
+    gmail: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormdata({
+      ...formdata,
+      [name]: value,
+    });
+  };
+
+  const submitData = async (e) => {
+    e.preventDefault();
+    try {
+      axios.defaults.withCredentials = true;
+      const response = await axios.post(
+        " http://localhost:8000/api/v1/mentor/login",
+        formdata,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response);
+      console.log(response.data.data.accessToken);
+      localStorage.setItem("token", response.data.data.accessToken);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <p>LOGIN FORM FOR STUDENT</p>
+
+      <form onSubmit={submitData}>
+        <label>
+          email
+          <input
+            className="border border-black"
+            type="email"
+            name="gmail"
+            value={formdata.gmail}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label>
+          password
+          <input
+            className="border border-black"
+            type="password"
+            name="password"
+            value={formdata.password}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <button className="border border-black" type="submit">
+          LOGIN
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default MentorLogin;
---
+++ frontend/src/components/MentorRegistration.js
@@ -0,0 +1,317 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const MentorRegistration = () => {
+  const [formdata, setFormdata] = useState({
+    firstname: "",
+    lastname: "",
+    password: "",
+    age: 0,
+    phone: "",
+    gmail: "",
+    place: "",
+    language_spoken: "",
+    gender: "",
+    education: [
+      {
+        collegeName: "",
+        degreeName: "",
+        passoutYear: "",
+      },
+    ],
+    company: "",
+    role: "",
+    field: "",
+    yearofExp: "",
+    skills: "",
+    fees: " ",
+  });
+
+  const handlechange = (e) => {
+    const { name, value } = e.target;
+    const [field, index] = name.split('.');
+
+    if (index) {
+      const updatedEducation = [...formdata.education];
+      updatedEducation[0][field] = value;
+
+      setFormdata({
+        ...formdata,
+        education: updatedEducation,
+      });
+    } else {
+      setFormdata({
+        ...formdata,
+        [name]: value,
+      });
+    }
+  };
+
+  const submitData = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/mentor/register",
+        formdata,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //-----------------------------------------------------------------
+  return (
+    <div>
+      <form onSubmit={submitData}>
+        <label>
+          firstname:
+          <input
+            className="border border-black"
+            type="text"
+            name="firstname"
+            value={formdata.firstname}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          lastname:
+          <input
+            className="border border-black"
+            type="text"
+            name="lastname"
+            value={formdata.lastname}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          password:
+          <input
+            className="border border-black"
+            type="password"
+            name="password"
+            value={formdata.password}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          Age:
+          <input
+            className="border border-black"
+            type="number"
+            name="age"
+            value={formdata.age}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          Phone:
+          <input
+            className="border border-black"
+            type="text"
+            name="phone"
+            value={formdata.phone}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          Gmail:
+          <input
+            className="border border-black"
+            type="email"
+            name="gmail"
+            value={formdata.gmail}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          place:
+          <input
+            className="border border-black"
+            type="text"
+            name="place"
+            value={formdata.place}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          language_spoken:
+          <input
+            className="border border-black"
+            type="text"
+            name="language_spoken"
+            value={formdata.language_spoken}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          gender:
+          <input
+            className="border border-black"
+            type="text"
+            name="gender"
+            value={formdata.gender}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          skills:
+          <input
+            className="border border-black"
+            type="text"
+            name="skills"
+            value={formdata.skills}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <h3>Education:</h3>
+        <label>
+          College Name:
+          <input
+            className="border border-black"
+            type="text"
+            name="collegeName.0"
+            value={formdata.education[0].collegeName}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          Degree Name:
+          <input
+            className="border border-black"
+            type="text"
+            name="degreeName.0"
+            value={formdata.education[0].degreeName}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          Passout Year:
+          <input
+            className="border border-black"
+            type="number"
+            name="passoutYear.0"
+            value={formdata.education[0].passoutYear}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Company:
+          <input
+            className="border border-black"
+            type="text"
+            name="company"
+            value={formdata.company}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          Role:
+          <input
+            className="border border-black"
+            type="text"
+            name="role"
+            value={formdata.role}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          Field:
+          <input
+            className="border border-black"
+            type="text"
+            name="field"
+            value={formdata.field}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          yearofExp:
+          <input
+            className="border border-black"
+            type="number"
+            name="yearofExp"
+            value={formdata.yearofExp}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+
+        <label>
+          fees:
+          <input
+            className="border border-black"
+            type="number"
+            name="fees"
+            value={formdata.fees}
+            onChange={handlechange}
+            required
+          />
+        </label>
+        <br />
+
+        <button className="border border-black" type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default MentorRegistration;
