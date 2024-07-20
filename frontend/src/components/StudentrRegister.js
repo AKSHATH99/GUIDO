ae368aad54cb3ddcd0da1e103c84c7f16c54a7ce
AKSHATH P <akshathpkk@gmail.com>
2024-07-20 12:00:33 +0530

    added tailwind

--- frontend/src/components/StudentrRegister.js
+++ frontend/src/components/StudentrRegister.js
@@ -29,7 +29,7 @@ const StudentRegister = () => {
 
     if (index) {
       const updatedEducation = [...formdata.education];
-      updatedEducation[0][field] = value;
+      updatedEducation[index][field] = value;
 
       setFormdata({
         ...formdata,
@@ -46,7 +46,7 @@ const StudentRegister = () => {
   const submitData = async (e) => {
     e.preventDefault();
 
-    try {   
+    try {
       const response = await axios.post(
         "http://localhost:8000/api/v1/student/register",
         formdata,
@@ -64,198 +64,240 @@ const StudentRegister = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={submitData}>
-        <label>
-          firstname:
-          <input
-            className="border border-black"
-            type="text"
-            name="firstname"
-            value={formdata.firstname}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+    <>
+      <div className=" border bg-gradient-to-r from-gray-100 to-gray-300">
+        <h1 className="text-4xl text-center mt-32   h-20 flex items-center justify-center ">
+          {" "}
+          FIND YOURSELF THE RIGHT{" "}
+          <span className="text-rose-500 mx-3 font-bold underline">
+            {" "}
+            MENTOR{" "}
+          </span>{" "}
+          {/* YOU NEVER HAD{" "} */}
+        </h1>
+        <h1 className="text-4xl text-center mt-3     h-20 flex items-center justify-center ">
+          {" "}
+          REGISTER YOURSELF AS A STUDENT AT{" "}
+          <span className="text-rose-500 ml-3 font-bold underline">
+            {" "}
+            GUIDO{" "}
+          </span>{" "}
+        </h1>
+        <div className="m-96 mt-24 border rounded-xl shadow-2xl bg-white ">
+          <form onSubmit={submitData}>
+            <h1 className="m-10 mb-0 text-xl ">PERSONAL INFORMATION</h1>
+            <label className="m-10  text-xl">
+              Name
+              <input
+                className="border  m-10 h-10 w-1/3 rounded-md  border-gray-200 bg-gray-200 p-3"
+                placeholder="Firstname"
+                type="text"
+                name="firstname"
+                value={formdata.firstname}
+                onChange={handlechange}
+                required
+              />
+            </label>
+            <label className=" text-xl">
+              <input
+                className="border border-gray-200 bg-gray-200 m-10 -ml-5 mb-0 h-10 w-1/3  rounded-md p-3"
+                type="text"
+                placeholder="Lastname"
+                name="lastname"
+                value={formdata.lastname}
+                onChange={handlechange}
+                required
+              />
+            </label>
+            <br />
 
-        <label>
-          lastname:
-          <input
-            className="border border-black"
-            type="text"
-            name="lastname"
-            value={formdata.lastname}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+            <label className=" m-8 ml-10 text-xl">
+              Gmail
+              <input
+                className="border border-gray-200 bg-gray-200 m-7 h-10 w-1/2 mt-0 ml-10 rounded-md p-3"
+                type="email"
+                name="gmail"
+                value={formdata.gmail}
+                onChange={handlechange}
+                placeholder="abc@gmail.com"
+                required
+              />
+            </label>
+            <br />
 
-        <label>
-          password:
-          <input
-            className="border border-black"
-            type="password"
-            name="password"
-            value={formdata.password}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+            <label className=" m-8 ml-10 text-xl">
+              Phone:
+              <input
+                className="border border-gray-200 bg-gray-200 m-7 h-10 w-1/2 mt-0  rounded-md p-3"
+                type="text"
+                name="phone"
+                value={formdata.phone}
+                onChange={handlechange}
+                placeholder="91 00000 00000 "
+                required
+              />
+            </label>
+            <br />
 
-        <label>
-          Age:
-          <input
-            className="border border-black"
-            type="number"
-            name="age"
-            value={formdata.age}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+            <div className="-mt-5">
+              <label className=" m-8 ml-11  text-xl">
+                Age
+                <input
+                  className="border border-gray-200 bg-gray-200 m-7 ml-12 h-10 w-1/4 rounded-md p-3"
+                  type="number"
+                  name="age"
+                  value={formdata.age}
+                  onChange={handlechange}
+                  placeholder="0"
+                  required
+                />
+              </label>
 
-        <label>
-          Phone:
-          <input
-            className="border border-black"
-            type="text"
-            name="phone"
-            value={formdata.phone}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+              <label className="m-8 text-xl">
+                Gender
+                <input
+                  className="border border-gray-200 bg-gray-200 m-7 h-10 w-1/4  rounded-md p-3"
+                  type="text"
+                  name="gender"
+                  value={formdata.gender}
+                  placeholder="Male / Female "
+                  onChange={handlechange}
+                  required
+                />
+              </label>
+            </div>
+            <br />
 
-        <label>
-          Gmail:
-          <input
-            className="border border-black"
-            type="email"
-            name="gmail"
-            value={formdata.gmail}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+            <label className="m-8 ml-10 -mt-10 text-xl">
+              Place
+              <input
+                className="border border-gray-200 bg-gray-200 m-7 h-10 w-1/2 mt-0 ml-10  rounded-md p-3"
+                type="text"
+                name="place"
+                value={formdata.place}
+                onChange={handlechange}
+                placeholder="Place"
+                required
+              />
+            </label>
+            <br />
 
-        <label>
-          place:
-          <input
-            className="border border-black"
-            type="text"
-            name="place"
-            value={formdata.place}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+            <label className=" m-8 ml-10 text-xl">
+              Languages You Speak
+              <input
+                className="border border-gray-200 bg-gray-200 m-7 h-10 w-1/2 mt-0  rounded-md p-3"
+                type="text"
+                name="language_spoken"
+                value={formdata.language_spoken}
+                onChange={handlechange}
+                placeholder="Hindi , English , Spanish"
+                required
+              />
+            </label>
+            <br />
 
-        <label>
-          language_spoken:
-          <input
-            className="border border-black"
-            type="text"
-            name="language_spoken"
-            value={formdata.language_spoken}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+            {/* ---------------------------------------------------- */}
 
-        <label>
-          gender:
-          <input
-            className="border border-black"
-            type="text"
-            name="gender"
-            value={formdata.gender}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+            <h1 className="m-10 mb-3 text-xl">EDUCATIONAL INFORMATION</h1>
 
-        <label>
-          skills:
-          <input
-            className="border border-black"
-            type="text"
-            name="skills"
-            value={formdata.skills}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+            <label className=" m-8 ml-10 text-xl ">
+              Skills
+              <input
+                className="border border-gray-200 bg-gray-200 m-7 h-10 w-1/2 mt-0 ml-28 rounded-md p-3"
+                type="text"
+                name="skills"
+                value={formdata.skills}
+                onChange={handlechange}
+                placeholder="PYTHON , JAVA , GO "
+                required
+              />
+            </label>
+            <br />
 
-        <h3>Education:</h3>
-        <label>
-          College Name:
-          <input
-            className="border border-black"
-            type="text"
-            name="collegeName.0"
-            value={formdata.education[0].collegeName}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+            {/* <h3>Education:</h3> */}
+            <label className=" m-8 ml-10 text-xl ">
+              College Name:
+              <input
+                className="border border-gray-200 bg-gray-200 m-7 h-10 w-1/2 mt-0  rounded-md p-3"
+                type="text"
+                name="collegeName.0"
+                value={formdata.education[0].collegeName}
+                onChange={handlechange}
+                placeholder="ABC University"
+                required
+              />
+            </label>
+            <br />
 
-        <label>
-          Degree Name:
-          <input
-            className="border border-black"
-            type="text"
-            name="degreeName.0"
-            value={formdata.education[0].degreeName}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+            <label className=" m-8 ml-10 text-xl ">
+              Degree Name:
+              <input
+                className="border border-gray-200 bg-gray-200 m-7 h-10 w-1/2 mt-0  rounded-md p-3"
+                type="text"
+                name="degreeName.0"
+                value={formdata.education[0].degreeName}
+                onChange={handlechange}
+                placeholder="Bachelors of Computer Science"
+                required
+              />
+            </label>
+            <br />
 
-        <label>
-          Passout Year:
-          <input
-            className="border border-black"
-            type="number"
-            name="passoutYear.0"
-            value={formdata.education[0].passoutYear}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+            <label className=" m-8 ml-10 text-xl ">
+              Passout Year:
+              <input
+                className="border border-gray-200 bg-gray-200 m-7 h-10 w-1/2 mt-0 ml-10 rounded-md p-3"
+                type="number"
+                name="passoutYear.0"
+                value={formdata.education[0].passoutYear}
+                onChange={handlechange}
+                placeholder="2025"
+                required
+              />
+            </label>
+            <br />
 
-        <label>
-          Current Year:
-          <input
-            className="border border-black"
-            type="number"
-            name="currentYear.0"
-            value={formdata.education[0].currentYear}
-            onChange={handlechange}
-            required
-          />
-        </label>
-        <br />
+            <label className=" m-8 ml-10 text-xl ">
+              Current Year:
+              <input
+                className="border border-gray-200 bg-gray-200 m-7 h-10 w-1/2 mt-0 ml-10 rounded-md p-3"
+                type="number"
+                name="currentYear.0"
+                value={formdata.education[0].currentYear}
+                onChange={handlechange}
+                placeholder="2"
+                required
+              />
+            </label>
+            <br />
 
-        <button className="border border-black" type="submit">
-          Register
-        </button>
-      </form>
-    </div>
+            <h1 className="m-10 mb-3 text-xl">CREATE A PASSWORD</h1>
+
+            <label className=" m-8 ml-10 text-xl">
+              {/* Password */}
+              <input
+                className="border border-gray-200 bg-gray-200 m-7 h-10 w-1/2 mt-0 ml-1  rounded-md p-3"
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={formdata.password}
+                onChange={handlechange}
+                required
+              />
+            </label>
+            <br />
+
+            <button
+              className="border border-green-100 bg-green-100  w-3/4 ml-28 h-12 rounded-2xl m-5 text-2xl hover:bg-green-300"
+              type="submit"
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
 export default StudentRegister;
-      
