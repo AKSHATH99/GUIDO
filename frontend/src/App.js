77b7e39dac93759adebbcf328bddc4985b66e512
AKSHATH P <akshathpkk@gmail.com>
2024-07-23 22:11:16 +0530

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,20 @@ import './App.css';
 import StudentAcount from './components/StudentAcount.js';
 import StudentLogin from './components/StudentLogin.js';
 import StudentRegister from './components/StudentrRegister.js';
+import MentorRegistration from './components/MentorRegistration.js';
+import MentorLogin from './components/MentorLogin.js';
+import MentorAccount from './components/MentorAccount.js';
 
 function App() {
   return (
     <div className="App">
       
       {/* <StudentRegister/>  */}
-      <StudentLogin/>
-      <StudentAcount/>
+      {/* <StudentLogin/>
+      <StudentAcount/> */}
+      {/* <MentorRegistration/> */}
+      {/* <MentorLogin/> */}
+      <MentorAccount/>  
       
     </div>
   );
