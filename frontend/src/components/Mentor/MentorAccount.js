0be13862c202006d268ce721d49e7b8834376de6
AKSHATH P <akshathpkk@gmail.com>
2024-07-24 23:51:33 +0530

    added button to add review and added comments to code

--- frontend/src/components/Mentor/MentorAccount.js
+++ frontend/src/components/Mentor/MentorAccount.js
@@ -1,13 +1,55 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import FeedbackComponent from "./FeedbackComponent";
-import HeaderComponent from "../HeaderComponent.js"
-import Shimmer from "../Shimmer.js"
+import HeaderComponent from "../HeaderComponent.js";
+import Shimmer from "../Shimmer.js";
+import Popup from "reactjs-popup";
 
 const MentorAccount = () => {
   const [mentorData, setMentorData] = useState("");
+  const [bookStatus, setBookStatus] = useState(false);
+  const [reviewdata , setReviewData] = useState("");
+  const [ReviewSubmited , setReviewSubmited] = useState(true);
   // const [st]
 
+  //Function to book session
+  const bookingHandler = () => {
+    setBookStatus(true);
+    console.log(bookStatus);
+  };
+
+  //Function to handle review input
+  const handleChange=(e)=>{
+    const {name , value} = e.target;
+    setReviewData(value);
+    console.log(value , "from handlechange")
+  }
+
+  //Function to submit review
+  const HandleReviewSubmit = async(e)=>{
+    e.preventDefault();
+    try {
+      axios.defaults.withCredentials = true;
+      const token = localStorage.getItem("token");
+
+      const response = await axios.post("",reviewdata,{
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },})
+
+        if(response){
+          setReviewSubmited(true);
+          console.log("SUBMITTED REVIEW",response)
+
+        }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //Function to fetch user data
   const fetchData = async () => {
     try {
       axios.defaults.withCredentials = true;
@@ -23,128 +65,184 @@ const MentorAccount = () => {
         }
       );
 
-      setMentorData(response.data.data)
+      setMentorData(response.data.data);
       console.log(response.data.data.firstname);
-      console.log(mentorData.education[0].collegeName)
+      console.log(mentorData.education[0].collegeName);
     } catch (error) {
       console.log(error);
     }
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[])
-  
+  }, []);
+
+
 
   // -------------------------------------------------------------------
   return (
     <>
-    <HeaderComponent/>
-    {mentorData?
-    <div className="bg-slate-100">
-      <div className="border bg-white m-12  ">
-        <div className="  flex ">
-          <div className="flex m-10">
-            <div>
-              <img
-                className="rounded-full w-36 h-36"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS3mr0rOaAemqvSNKmzBD-I6mcpod9HFQuCw&usqp=CAU"
-              />
+      <HeaderComponent />
+      {mentorData ? (
+        <div className="bg-slate-100">
+          <div className="border bg-white m-12  ">
+            <div className="  flex ">
+              <div className="flex m-10">
+                <div>
+                  <img
+                    className="rounded-full w-36 h-36"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS3mr0rOaAemqvSNKmzBD-I6mcpod9HFQuCw&usqp=CAU"
+                  />
+                </div>
+                <div className="mt-10 m-5 ">
+                  <p className="font-bold text-4xl"> {mentorData.firstname}</p>
+                  <p className="text-xl mt-5 flex">
+                    {mentorData.age} y/o{" "}
+                    <img className="ml-4" src="/images/location.png" />{" "}
+                    <span className="">{mentorData.place} </span>{" "}
+                  </p>
+                </div>
+              </div>
+
+              <div className="ml-36 mt-24 flex">
+                <div>
+                  <img className="h-10 mr-3 mt-6" src="/images/grag.png" />
+                </div>
+                <div>
+                  <p className="text-4xl font-bold"> 1000 + </p>
+                  <p className="text-xl mt-2">Students Mentored</p>
+                </div>
+              </div>
+
+              <div className="ml-52 mt-24 flex">
+                <div>
+                  <img className="h-12 mr-3 mt-2" src="/images/org.png" />
+                </div>
+                <div>
+                  <p className="text-4xl font-bold"> {mentorData.yearofExp} </p>
+                  <p className="text-xl mt-2">
+                    Years of Industrial Experiences
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="mt-10 m-5 ">
-              <p className="font-bold text-4xl"> {mentorData.firstname}</p>
-              <p className="text-xl mt-5 flex">
-              {mentorData.age} y/o <img className="ml-4" src="/images/location.png" />{" "}
-                <span className="">{mentorData.place} </span>{" "}
-              </p>
+
+            <div className="flex">
+              <div className="border w-1/3 ml-[450px] m-5 rounded-3xl bg-green-300 hover:bg-green-500">
+                <button
+                  className=" p-5 w-full   text-xl font-bold"
+                  onClick={bookingHandler}
+                >
+                  BOOK A SESSION | ₹ {mentorData.fees}
+                </button>
+              </div>
+              {/* Code to display popup to enter review  */}
+              {bookStatus && (
+                <Popup
+                  trigger={
+                    <div className="border m-5 flex items-center justify-center ml-56 w-48 font-bold rounded-lg">
+                      <img
+                        className="w-10 m-3 h-10"
+                        src="/images/review2.png"
+                      />
+                      <p>ADD REVIEW</p>
+                    </div>
+                  }
+                  position="bottom center"
+                >
+                  <div className="bg-white border shadow-lg">
+                    <textarea
+                      type="text"
+                      cols="45"
+                      rows="10"
+                      value={reviewdata}
+                      name = "review"
+                      placeholder="Write up your review and experience here to help others "
+                      className="m-5 border border-black"
+                    />
+                    <br />
+                    <button
+                      className="border m-5 border-green-300 h-10 w-36 ml-28 bg-green-100"
+                      onClick={HandleReviewSubmit}
+                    >
+                      SUBMIT{" "}
+                    </button>
+                  </div>
+                </Popup>
+              )}
             </div>
           </div>
 
-          <div className="ml-36 mt-24 flex">
-            <div>
-              <img className="h-10 mr-3 mt-6" src="/images/grag.png" />
-            </div>
-            <div>
-              <p className="text-4xl font-bold"> 1000 + </p>
-              <p className="text-xl mt-2">Students Mentored</p>
-            </div>
-          </div>
-
-          <div className="ml-52 mt-24 flex">
-            <div>
-              <img className="h-12 mr-3 mt-2" src="/images/org.png" />
-            </div>
-            <div>
-              <p className="text-4xl font-bold"> {mentorData.yearofExp} </p>
-              <p className="text-xl mt-2">Years of Industrial Experiences</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="border w-1/3 ml-[450px] m-5 rounded-3xl bg-green-300 hover:bg-green-500">
-          <button className=" p-5 w-full   text-xl font-bold">BOOK A SESSION   |    ₹ {mentorData.fees}</ button>
-        </div>
-      </div>
-
-      <div className="border flex bg-white m-12">
-        <div className=" w-1/3 border-r">
-          <p className="text-3xl m-7 font-bold"> About Me </p>
-          {/* <hr className="border border-black" /> */}
-          <p className="m-5 text-xl leading-loose">
-            {/* I am a tech mentor with a strong background in technology and 8
+          <div className="border flex bg-white m-12">
+            <div className=" w-1/3 border-r">
+              <p className="text-3xl m-7 font-bold"> About Me </p>
+              {/* <hr className="border border-black" /> */}
+              <p className="m-5 text-xl leading-loose">
+                {/* I am a tech mentor with a strong background in technology and 8
             years of experience. Currently, I work as an SDE 2 at Google. I love
             helping others understand and succeed in the tech industry. Whether
             you're new to tech or looking to advance, I'm here to guide and
             support you with practical advice and encouragement. Let's work
             together to achieve your goals and grow your skills. */}
-            {mentorData.bio}
-          </p>
-          {/* <hr className="border border-gray-200" /> */}
-          {/* <p className="text-3xl m-7 font-bold">PROFESSIONAL DETAILS</p> */}
-          <div className="m-7">
-          <p className="text-xl ">Works At : </p>
-          <p className="text-4xl font-semibold">{mentorData.company} .  </p>
-          </div>
+                {mentorData.bio}
+              </p>
+              {/* <hr className="border border-gray-200" /> */}
+              {/* <p className="text-3xl m-7 font-bold">PROFESSIONAL DETAILS</p> */}
+              <div className="m-7">
+                <p className="text-xl ">Works At : </p>
+                <p className="text-4xl font-semibold">
+                  {mentorData.company} .{" "}
+                </p>
+              </div>
 
-          <div className="m-7">
-          <p className="text-xl ">Working As : </p>
-          <p className="text-4xl font-semibold">{mentorData.role}  </p>
-          </div>
+              <div className="m-7">
+                <p className="text-xl ">Working As : </p>
+                <p className="text-4xl font-semibold">{mentorData.role} </p>
+              </div>
 
-          <div className="m-7">
-          <p className="text-xl ">Field of Expertise : </p>
-          <p className="text-4xl font-semibold">{mentorData.field}  </p>
-          </div>
+              <div className="m-7">
+                <p className="text-xl ">Field of Expertise : </p>
+                <p className="text-4xl font-semibold">{mentorData.field} </p>
+              </div>
 
-          <div className="m-7">
-          <p className="text-xl ">Skills : </p>
-          <p className="text-4xl font-semibold">{mentorData.skills}  </p>
-          </div>
+              <div className="m-7">
+                <p className="text-xl ">Skills : </p>
+                <p className="text-4xl font-semibold">{mentorData.skills} </p>
+              </div>
 
-          <div className="m-7 mt-28">
-          <p className="text-xl ">Languages I speak  : </p>
-          <p className="text-4xl font-semibold">{mentorData.language_spoken}  </p>
-          </div>
+              <div className="m-7 mt-28">
+                <p className="text-xl ">Languages I speak : </p>
+                <p className="text-4xl font-semibold">
+                  {mentorData.language_spoken}{" "}
+                </p>
+              </div>
 
-          <div className="m-7 mt-28">
-          <p className="text-xl "> Graduated From  : </p>
-          <p className="text-4xl font-semibold">{mentorData?.education[0]?.collegeName} , {mentorData?.education[0]?.passoutYear} </p>
-          <p className="text-xl font-semibold">{mentorData?.education[0]?.degreeName}</p>
+              <div className="m-7 mt-28">
+                <p className="text-xl "> Graduated From : </p>
+                <p className="text-4xl font-semibold">
+                  {mentorData?.education[0]?.collegeName} ,{" "}
+                  {mentorData?.education[0]?.passoutYear}{" "}
+                </p>
+                <p className="text-xl font-semibold">
+                  {mentorData?.education[0]?.degreeName}
+                </p>
+              </div>
+              {/* <hr className="border border-gray-200"/> */}
+            </div>
+
+            <div className="w-1/2">
+              <p className="text-3xl m-7 font-bold">REVIEWS & FEEDBACKS </p>
+              <FeedbackComponent />
+              <FeedbackComponent />
+              <FeedbackComponent />
+              <FeedbackComponent />
+            </div>
           </div>
-          {/* <hr className="border border-gray-200"/> */}
+          {/* <button onClick={fetchData} className="border border-black">FETCH DATA</button> */}
         </div>
-
-        <div className="w-1/2">
-          <p className="text-3xl m-7 font-bold">REVIEWS & FEEDBACKS </p>
-          <FeedbackComponent />
-          <FeedbackComponent />
-          <FeedbackComponent />
-          <FeedbackComponent />
-        </div>
-      </div>
-      {/* <button onClick={fetchData} className="border border-black">FETCH DATA</button> */}
-    </div>
-    :<Shimmer/>}
+      ) : (
+        <Shimmer />
+      )}
     </>
   );
 };
