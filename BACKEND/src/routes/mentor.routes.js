b2c908f78e39c14dcc80d23d332c7318a995ff33
AKSHATH P <akshathpkk@gmail.com>
2024-07-26 00:45:34 +0530

    route for fetchMentorByID added

--- BACKEND/src/routes/mentor.routes.js
+++ BACKEND/src/routes/mentor.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {  registerMentor, LoginMentor, fetchMentor , fetchAMentor} from "../controllers/mentor.controller.js";
+import {  registerMentor, LoginMentor, fetchMentor , fetchAMentor , fetchMentorByID} from "../controllers/mentor.controller.js";
 import multer from "multer";
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js";
@@ -30,5 +30,8 @@ router.route("/fetch").get(verifyJWT ,fetchMentor);
 
 // https://localhost:8000/api/mentor/find/:firstname
 router.route("/find/:firstname").get(verifyJWT , fetchAMentor);
+
+// https://localhost:8000/api/mentor/:id
+router.route("/:id").get(cors(corsOptions) ,fetchMentorByID)
 
 export default router;
