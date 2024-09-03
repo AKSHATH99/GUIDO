c35176c9a9864e06c14c35b0c3b5f18b691eea39
Akshath P <akshathpkk@gmail.com>
2024-09-03 21:36:24 +0530

    deployment issue fix

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -5,24 +5,30 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 // CORS Middleware (place this at the top, before routes)
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://guido-frontend.vercel.app/'); // Replace with your frontend's origin
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Optional: Allow specific headers
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://guido-frontend.vercel.app/'); // Replace with your frontend's origin
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Optional: Allow specific headers
+//     next();
+//   });
+// const corsOptions = {
+//   origin: "http://localhost:3000", // your frontend domain
+//   credentials: true, // allow credentials (cookies)
+// };
+// app.use(cors({ origin: 'https://guido-frontend.vercel.app/', credentials: true }))
+
+// app.use(
+//   cors({ origin: ["https://guido-frontend.vercel.app"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials:true,
+// })
+// );
+
 const corsOptions = {
-  origin: "http://localhost:3000", // your frontend domain
+  origin: "https://guido-frontend.vercel.app", // Replace with your frontend's origin
   credentials: true, // allow credentials (cookies)
 };
-app.use(cors({ origin: 'https://guido-frontend.vercel.app/', credentials: true }))
 
-app.use(
-  cors({ origin: ["https://guido-frontend.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials:true,
-})
-);
 
 app.use(express.json({ limit: "1600000" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
