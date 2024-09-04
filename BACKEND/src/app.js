c7f10d8b224d0824cafd783fe2d6aa7809135874
Akshath P <akshathpkk@gmail.com>
2024-09-04 19:45:31 +0530

    deployment issue fix

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -6,6 +6,7 @@ const app = express();
 
 // CORS Middleware (place this at the top, before routes)
 app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Origin', 'https://guido-frontend.vercel.app/'); // Replace with your frontend's origin
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Optional: Allow specific headers
